2f1d2e66c1132f81155eb3f4ec2235b392482c3a
Yohann-Moy <yohann@mbp-de-yohann.home>
2023-08-31 20:51:13 +0200

    Legers ajustements...

--- base/index.php
+++ base/index.php
@@ -3,7 +3,7 @@
     require_once("./webfiles/php/header.php");
 ?>
 
-<p>Cliquez <a href="./base(corrige)/webfiles/php/books/all.php">ici</a> pour accéder à notre bibliothèque</p>
+<p>Cliquez <a href="./base/webfiles/php/books/all.php">ici</a> pour accéder à notre bibliothèque</p>
 
 <!-- Correction V1 -->
 <!-- Code corrigé pour faciliter l'implémentation d'un MVC procédural -->
